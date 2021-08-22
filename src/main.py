b9185106710230c8f1d96e654ed6bab1eebddef3
djvoelker <djvoelker28@outlook.com>
2021-08-22 14:11:47 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,25 +2,19 @@
 Created on Aug 21, 2021
 
 @author: djvoe
-'''
 
-"""
 Things to add:
-    - Gui
+    - GUI
     - Error handling when input isn't an integer between 1 - 9
-    - Ability to play game again after completion
     - Not have computer respond instantly
     - Not print out different board for each move
-    - Not make computer block your win if it has a chance to win
-    - Make sure scan_board works
-"""
-
+    - Choose who goes first and who plays as X and who plays as O
+'''
 
 from operator import itemgetter
 from random import randint
 from time import sleep
 
-print(" Type 'exit' to exit game")
 
 class game:
     def __init__(self):
@@ -32,13 +26,16 @@ class game:
         self.turns_played = 0
         while self.winner == False:
             self.player_turn()
-            if self.exit == True: return
+            if self.exit == True:
+                game.exit = True
+                return
         self.print_grid()
         print(self.winner)
-        sleep(5)
+
 
     def setup_grid(self):
         self.grid = [" " for _ in range(9)]
+        
         
     def print_grid(self):
         print("\n")
@@ -47,32 +44,40 @@ class game:
             if r < 2: print("    -----------")
         print("\n\n------------------------------------------------------------")
         
+        
     def player_turn(self):
         player_sel = input("\n What box (from 1 - 9) would you like to draw an X in? ")
         if player_sel.casefold() == "exit": self.exit = True
-        if self.grid[int(player_sel)-1] == " ":
-            self.grid[int(player_sel)-1] = "X"
-            self.winner = self.check_for_winner('X')
-            if self.winner == 'X': 
-                self.winner = " You win!"
-                return
-            elif self.winner == " The game is a tie.":
-                return
-            self.turns_played += 1
-                    
-            opponent_move = self.scan_board('X')
-            if opponent_move == True: self.scan_board('O')
-            self.winner = self.check_for_winner('O')
-            if self.winner == 'O': 
-                self.winner = " You lose!"
-                return
-            elif self.winner == " The game is a tie.":
-                return
-            self.turns_played += 1
-            
-            self.print_grid()
-            
-        else: print("\n That space is already filled in!\n")
+        try:
+            if self.grid[int(player_sel)-1] == " ":
+                
+                #player turn
+                self.grid[int(player_sel)-1] = "X"
+                self.winner = self.check_for_winner('X')
+                if self.winner == 'X': 
+                    self.winner = " You win!"
+                    return
+                elif self.winner == " The game is a tie.":
+                    return
+                self.turns_played += 1
+                
+                #computer turn        
+                self.scan_board('O')
+                self.winner = self.check_for_winner('O')
+                if self.winner == 'O': 
+                    self.winner = " You lose!"
+                    return
+                elif self.winner == " The game is a tie.":
+                    return
+                self.turns_played += 1
+                
+                self.print_grid()
+                
+            else: print("\n That space is already filled in!\n")
+        
+        except (ValueError, IndexError):
+            print("\n Please type an integer from 1 - 9 or type 'exit' to exit game.")
+        
         
     def scan_board(self, a):
         #check diagonals
@@ -91,6 +96,7 @@ class game:
         if itemgetter(0,4)(self.grid) == (a, a) and self.grid[8] == " ":
             self.grid[8] = 'O'
             return
+        
         #check rows
         for r in range(3):
             if itemgetter(3*r, 3*r+1)(self.grid) == (a, a) and self.grid[3*r+2] == " ":
@@ -102,6 +108,7 @@ class game:
             if itemgetter(3*r, 3*r+2)(self.grid) == (a, a) and self.grid[3*r+1] == " ":
                 self.grid[3*r+1] = 'O'
                 return
+        
         #check columns
         for c in range(3):
             if itemgetter(c, 3+c)(self.grid) == (a, a) and self.grid[6+c] == " ":
@@ -115,7 +122,7 @@ class game:
                 return
         
         #fill in random spot if there is no opportunity to block player
-        if a == 'O':
+        if a == 'X':
             squares = [x for x in range(9)]
             while True:
                 n = randint(0,len(squares)-1)
@@ -124,7 +131,10 @@ class game:
                     return
                 del squares[n]
         
-        return True
+        #if no opportunity for computer to win, it will then see if the player has an opportunity to win and will block them.
+        #if the player does not have the opportunity to win, the computer will then place an O at a random empty spot on the board.
+        self.scan_board('X')
+    
     
     def check_for_winner(self, x):
         if (
@@ -141,6 +151,28 @@ class game:
             return x
         elif self.turns_played > 7: return " The game is a tie."
         else: return False
-        
-        
-game()
+
+
+def play_tic_tac_toe():
+    game.exit = False
+    print(" Welcome to Tic-Tac-Toe. Type 'exit' to exit game.")
+    game()
+    if game.exit == True:
+        print("\n Exiting game . . . ")
+        sleep(2)
+        return
+    while True:
+        sleep(2)
+        p = str(input("\nPlay again? Y/N: ")).casefold()
+        if p == "y":
+            print("\n\n")
+            game()
+        elif p == "n" or p == "exit":
+            print("\n Exiting game . . . ")
+            sleep(2)
+            return
+        else: print("\nPlease type Y/N.")
+
+
+play_tic_tac_toe()
+
