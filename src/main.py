e5266e63c5c90ac45dbec13f124a50ba24eeda62
djvoelker <djvoelker28@outlook.com>
2021-08-21 23:05:49 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,57 +4,79 @@ Created on Aug 21, 2021
 @author: djvoe
 '''
 
+"""
+Things to add:
+    - Gui
+    - Error handling when input isn't an integer between 1 - 9
+    - Ability to play game again after completion
+    - Not have computer respond instantly
+    - Not print out different board for each move
+    - Not make computer block your win if it has a chance to win
+    - Make sure scan_board works
+"""
+
+
 from operator import itemgetter
 from random import randint
+from time import sleep
+
+print(" Type 'exit' to exit game")
 
 class game:
     def __init__(self):
-        self.setup_grid()
+        self.grid = [x+1 for x in range(9)]
         self.print_grid()
+        self.setup_grid()
         self.winner = False
+        self.exit = False
         self.turns_played = 0
         while self.winner == False:
             self.player_turn()
+            if self.exit == True: return
         self.print_grid()
         print(self.winner)
-           
+        sleep(5)
+
     def setup_grid(self):
         self.grid = [" " for _ in range(9)]
         
     def print_grid(self):
+        print("\n")
         for r in range(3):    
-            print(str(self.grid[3*r+0]) + " | " + str(self.grid[3*r+1]) + " | " + str(self.grid[3*r+2]))
-            if r < 2: print("---------")
-            
+            print("     " + str(self.grid[3*r+0]) + " | " + str(self.grid[3*r+1]) + " | " + str(self.grid[3*r+2]))
+            if r < 2: print("    -----------")
+        print("\n\n------------------------------------------------------------")
+        
     def player_turn(self):
-        player_sel = int(input("What box (from 1 - 9) would you like to draw an X in? "))
-        if self.grid[player_sel-1] == " ":
-            self.grid[player_sel-1] = "X"
+        player_sel = input("\n What box (from 1 - 9) would you like to draw an X in? ")
+        if player_sel.casefold() == "exit": self.exit = True
+        if self.grid[int(player_sel)-1] == " ":
+            self.grid[int(player_sel)-1] = "X"
             self.winner = self.check_for_winner('X')
             if self.winner == 'X': 
-                self.winner = "You win!"
+                self.winner = " You win!"
                 return
-            elif self.winner == "The game is a tie.":
+            elif self.winner == " The game is a tie.":
                 return
             self.turns_played += 1
                     
             opponent_move = self.scan_board('X')
-            if opponent_move != None: opponent_move('O')
+            if opponent_move == True: self.scan_board('O')
             self.winner = self.check_for_winner('O')
             if self.winner == 'O': 
-                self.winner = "You lose!"
+                self.winner = " You lose!"
                 return
-            elif self.winner == "The game is a tie.":
+            elif self.winner == " The game is a tie.":
                 return
             self.turns_played += 1
             
             self.print_grid()
             
-        else: print("That space is already filled in!")
+        else: print("\n That space is already filled in!\n")
         
     def scan_board(self, a):
         #check diagonals
-        if itemgetter(0,8)(self.grid) == (a, a) or itemgetter(2,6)(self.grid) == (a, a) and self.grid[4] == " ":
+        if (itemgetter(0,8)(self.grid) == (a, a) and self.grid[4] == " ") or (itemgetter(2,6)(self.grid) == (a, a) and self.grid[4] == " "):
             self.grid[4] = 'O'
             return
         if itemgetter(4,8)(self.grid) == (a, a) and self.grid[0] == " ":
@@ -93,13 +115,16 @@ class game:
                 return
         
         #fill in random spot if there is no opportunity to block player
-        squares = [x for x in range(9)]
-        while True:
-            n = randint(0,len(squares)-1)
-            if self.grid[squares[n]] == " ":
-                self.grid[squares[n]] = 'O'
-                return
-            del squares[n]
+        if a == 'O':
+            squares = [x for x in range(9)]
+            while True:
+                n = randint(0,len(squares)-1)
+                if self.grid[squares[n]] == " ":
+                    self.grid[squares[n]] = 'O'
+                    return
+                del squares[n]
+        
+        return True
     
     def check_for_winner(self, x):
         if (
@@ -114,7 +139,7 @@ class game:
                 or itemgetter(2,4,6)(self.grid) == (x, x, x)
             ):
             return x
-        elif self.turns_played > 7: return "The game is a tie."
+        elif self.turns_played > 7: return " The game is a tie."
         else: return False
         
         
