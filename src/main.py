32ca8bd391d1641a9ad67bf087715c015f49d602
djvoelker <djvoelker28@outlook.com>
2021-08-23 00:55:19 -0400

    almost done fixing algorithm

--- src/main.py
+++ src/main.py
@@ -56,22 +56,28 @@ class game:
                     
                     #player turn
                     self.grid[int(player_sel)-1] = "X"
-                    self.winner = self.check_for_winner('X')
-                    if self.winner == 'X': 
-                        self.winner = " You win!"
-                        return
-                    elif self.winner == " The game is a tie.":
-                        return
+                    #check if player won
+                    if self.turns_played > 3:
+                        self.scan_board('X', 'O', True)
+                        if self.winner == 'X': 
+                            self.winner = " You win!"
+                            return
+                        elif self.winner == " The game is a tie.":
+                            return
+                        
                     self.turns_played += 1
-                    
-                    #computer turn        
-                    self.scan_board('O')
-                    self.winner = self.check_for_winner('O')
-                    if self.winner == 'O': 
-                        self.winner = " You lose!"
-                        return
-                    elif self.winner == " The game is a tie.":
-                        return
+                    """put the above and below code into a function"""                    
+                    #computer turn
+                    self.scan_board('O','O', False)
+                    #check if computer won
+                    if self.turns_played > 3:
+                        self.scan_board('O', 'X', True)
+                        if self.winner == 'O': 
+                            self.winner = " You lose!"
+                            return
+                        elif self.winner == " The game is a tie.":
+                            return
+                        
                     self.turns_played += 1
                     
                     self.print_grid()
@@ -82,50 +88,25 @@ class game:
                 print("\n Please type an integer from 1 - 9 or type 'exit' to exit game.")
         
         
-    def scan_board(self, a):
-        #check diagonals
-        if (itemgetter(0,8)(self.grid) == (a, a) and self.grid[4] == " ") or (itemgetter(2,6)(self.grid) == (a, a) and self.grid[4] == " "):
-            self.grid[4] = 'O'
-            return
-        if itemgetter(4,8)(self.grid) == (a, a) and self.grid[0] == " ":
-            self.grid[0] = 'O'
-            return
-        if itemgetter(4,6)(self.grid) == (a, a) and self.grid[2] == " ":
-            self.grid[2] = 'O'
-            return
-        if itemgetter(2,4)(self.grid) == (a, a) and self.grid[6] == " ":
-            self.grid[6] = 'O'
-            return
-        if itemgetter(0,4)(self.grid) == (a, a) and self.grid[8] == " ":
-            self.grid[8] = 'O'
-            return
-        
-        #check rows
-        for r in range(3):
-            if itemgetter(3*r, 3*r+1)(self.grid) == (a, a) and self.grid[3*r+2] == " ":
-                self.grid[3*r+2] = 'O'
-                return
-            if itemgetter(3*r+1, 3*r+2)(self.grid) == (a, a) and self.grid[3*r] == " ":
-                self.grid[3*r] = 'O'
-                return
-            if itemgetter(3*r, 3*r+2)(self.grid) == (a, a) and self.grid[3*r+1] == " ":
-                self.grid[3*r+1] = 'O'
-                return
-        
-        #check columns
-        for c in range(3):
-            if itemgetter(c, 3+c)(self.grid) == (a, a) and self.grid[6+c] == " ":
-                self.grid[6+c] = 'O'
-                return
-            if itemgetter(3+c, 6+c)(self.grid) == (a, a) and self.grid[c] == " ":
-                self.grid[c] = 'O'
-                return
-            if itemgetter(c, 6+c)(self.grid) == (a, a) and self.grid[3+c] == " ":
-                self.grid[3+c] = 'O'
-                return
-        
-        #fill in random spot if there is no opportunity to block player
-        if a == 'X':
+    def scan_board(self, i, j, check_win):
+        for m in range(3):
+            for n in range([2,3,3][m]):
+                a = [self.grid[[2*n+2*(2-n)*x, 3*n+x, n+3*x][m]] for x in range(3) if self.grid[[2*n+2*(2-n)*x, 3*n+x, n+3*x][m]] == i]
+                g = [self.grid[[2*n+2*(2-n)*x, 3*n+x, n+3*x][m]] for x in range(3)]
+                print(f"Set(a): {set(a)}, a: {a}, m: {m}, n: {n}, check_win: {check_win}")
+                if (check_win == True):
+                    if (i in g) and (len(set(a)) < (len(a)-1)) and (len(set(g).intersection({' ', j})) == 0):
+                        self.winner = i
+                        print("win")
+                if self.winner != False: return    
+                elif i in g and ' ' in g and len(set(a)) < len(a):
+                    #can I use character replacement in a string and set a var = self.grid[[2*n+2*(2-n)*x, 3*n+x, n+3*x][m]] and then swap in either x or g.index(" ") to decrease the number of times I have to type that long list generator index sequence?
+                    #also see if I can combine the check_for_winner function with this function
+                    self.grid[[2*n+2*(2-n)*g.index(" "), 3*n+g.index(" "), n+3*g.index(" ")][m]] = j
+                    return
+                
+        if i == 'O': self.scan_board('X','O', False)
+        else:
             squares = [x for x in range(9)]
             while True:
                 n = randint(0,len(squares)-1)
@@ -133,28 +114,16 @@ class game:
                     self.grid[squares[n]] = 'O'
                     return
                 del squares[n]
-        
-        #if no opportunity for computer to win, it will then see if the player has an opportunity to win and will block them.
-        #if the player does not have the opportunity to win, the computer will then place an O at a random empty spot on the board.
-        self.scan_board('X')
-    
-    
-    def check_for_winner(self, x):
-        if (
-                itemgetter(0,1,2)(self.grid) == (x, x, x)
-                or itemgetter(3,4,5)(self.grid) == (x, x, x)
-                or itemgetter(6,7,8)(self.grid) == (x, x, x)
-                or itemgetter(6,7,8)(self.grid) == (x, x, x)
-                or itemgetter(0,3,6)(self.grid) == (x, x, x)
-                or itemgetter(1,4,7)(self.grid) == (x, x, x)
-                or itemgetter(2,5,8)(self.grid) == (x, x, x)
-                or itemgetter(0,4,8)(self.grid) == (x, x, x)
-                or itemgetter(2,4,6)(self.grid) == (x, x, x)
-            ):
-            return x
-        elif self.turns_played > 7: return " The game is a tie."
+""" 
+    def check_for_winner(self, i, j):
+        for m in range(3):
+            for n in range([2,3,3][m]):
+                g = [self.grid[[2*n+2*(2-n)*x, 3*n+x, n+3*x][m]] for x in range(3)]
+                if i in g and len(set(g).intersection({' ', j})) == 0:
+                    return i
+        if self.turns_played > 7: return " The game is a tie."
         else: return False
-
+"""
 
 def play_tic_tac_toe():
     game.exit = False
@@ -166,15 +135,15 @@ def play_tic_tac_toe():
         return
     while True:
         sleep(2)
-        p = str(input("\nPlay again? Y/N: ")).casefold()
+        p = str(input("\n Play again? Y/N: ")).casefold()
         if p == "y":
-            print("\n\n")
+            print("\n")
             game()
         elif p == "n" or p == "exit":
             print("\n Exiting game . . . ")
             sleep(2)
             return
-        else: print("\nPlease type Y/N.")
+        else: print("\n Please type Y/N.")
 
 
 play_tic_tac_toe()
